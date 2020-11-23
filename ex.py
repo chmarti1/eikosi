714fe9a91ba68b13f5ba8fa7dfafd837c6d180a5
Chris Martin <crm28@psu.edu>
2020-11-23 16:02:45 -0500

    Rewrote Entry to use a simpler mandatory/optional system

--- ex.py
+++
@@ -1,8 +0,0 @@
-import pybib
-
-entry = pybib.ArticleEntry('test:2020')
-
-entry.author = pybib.AuthorList([['Christopher', 'Reed', 'Martin'], ['Leo', 'Sylvan', 'Martin']])
-entry.title = 'My first example output'
-entry.journal = 'Home code'
-entry.year = 2020
